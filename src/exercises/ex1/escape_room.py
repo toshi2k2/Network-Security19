11e6ed3b9b90ee0ad0b6d4871015598914df18d6
toshi2k2 <38487734+toshi2k2@users.noreply.github.com>
2019-01-30 15:34:44 -0500

    init

---
+++ src/exercises/ex1/escape_room.py
@@ -0,0 +1,22 @@
+import sys
+
+class EscapeRoom:
+
+    def start(self):
+        '''This method initializes the game'''
+
+    def command(self, command_string):
+        '''This command accepts the user's command within the game'''
+
+    def status(self):
+        '''Reports whether the users is "dead", "locked", or "escaped"'''
+
+
+def look():
+    pass
+
+def get():
+    pass
+
+def open():
+    pass
